fbad37c5fea3995c2a8b141e8a76a2615722011b
Marek Nagy <62247071@cuni.cz>
2020-10-14 12:33:02 +0200

    Added multiprocessing version

--- main-deap-multi.py
+++ main-deap-multi.py
@@ -14,27 +14,26 @@ from deap import creator
 from deap import tools
 
 # custom lib
-import min_span_tree
+#import min_span_tree
 
 random.seed(64)
 
 POINTS_NUM = 20
 POINTS_RANGE = 100
 
-best_path = None
-best_path_value = None
+paths = {}
 
 points = []
 for i in range(POINTS_NUM):
 	points.append( (random.randint(0, POINTS_RANGE), random.randint(0, POINTS_RANGE)) )
 
 # create nodes
-first_nodes = [ min_span_tree.Node(p[0], p[1], str(p)) for p in points ]
+#first_nodes = [ min_span_tree.Node(p[0], p[1], str(p)) for p in points ]
 # add neighbours
-for n in first_nodes:
-	for n2 in first_nodes:
-		if n2 != n:
-			n.add_neighbour(n2)
+#for n in first_nodes:
+#	for n2 in first_nodes:
+#		if n2 != n:
+#			n.add_neighbour(n2)
 
 def Index_node_in_points(node, points):
 	for i in range(len(points)):
@@ -42,8 +41,23 @@ def Index_node_in_points(node, points):
 			return i
 	return -1
 
-route = [ Index_node_in_points(node, points) for
-		node in min_span_tree.TSP_aproximate(first_nodes) ]
+# route = [ Index_node_in_points(node, points) for
+# 		node in min_span_tree.TSP_aproximate(first_nodes) ]
+
+class Warehouse:
+	X = None
+	Y = None
+	Path = None
+	Value = None
+	
+	def __init__(self, x: int, y: int):
+		self.X = x
+		self.Y = y
+	
+	def set_path(self, path: list, value: float):
+		if (self.Value > value):
+			self.Path = path
+			self.Value = value
 
 def GetDistance(point1, point2):
 	return sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 )
@@ -51,11 +65,14 @@ def GetDistance(point1, point2):
 distance_map = [ [ GetDistance(p1, p2) for p2 in points] for p1 in points]
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", array.array, typecode='f', fitness=creator.FitnessMin)
+#creator.create("Individual", array.array, typecode='f', fitness=creator.FitnessMin)
+creator.create("Individual", array.array, typecode='f', fitness=creator.FitnessMin,
+				path=None)
 
 toolbox = base.Toolbox()
 
 creator.create("TSPIndividual", array.array, typecode='i', fitness=creator.FitnessMin)
+
 
 def shuffle_TSP(route):
 	start_index = random.randint(-len(route) + 1, 0)
@@ -76,8 +93,7 @@ def evalTSP(TSPIndividual):
 	return distance,
 
 def evalFunc(individual):
-	global best_path_value, best_path
-	wh_x, wh_y = individual[0], individual[1]
+	global paths
 
 	toolbox = base.Toolbox()
 
@@ -118,8 +134,9 @@ def evalFunc(individual):
 	
 	# pop, logbook = algorithms.eaMuCommaLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN,
 	# 	stats = stats, halloffame=hof, verbose=False)
-
-	paths[str(individual)] = [ x for x in hof[0] ]
+	#paths[str(individual)] = hof[0]
+	individual.path = [ x for x in hof[0] ]
+	#print("Individual: {}\nPath: {}".format(individual, paths[str(individual)]) )
 	value = evalTSP(hof[0])
 	# if best_path_value is None or best_path_value > value[0]:
 	# 	best_path_value = value[0]
@@ -158,17 +175,18 @@ toolbox.register("mutate", mutateWarehouse)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 def main():
-
-	NGEN = 20 # amount of generations
-	MU = 40 # amount of individuals to select from each generation (possible parents)
-	LAMBDA = 60 # number of new children for each generation
+	global paths
+	NGEN = 5 # amount of generations
+	MU = 20 # amount of individuals to select from each generation (possible parents)
+	LAMBDA = 30 # number of new children for each generation
 	CXPB = 0.6 # probability of mating
 	MUTPB = 0.2 # mutation probability
 	
-	cpu_count = int(multiprocessing.cpu_count() / 2)
-	print("Running on {} CPU cores.".format(cpu_count))
-	pool = multiprocessing.Pool(processes=cpu_count)
-	toolbox.register("map", pool.map)
+	# cpu_count = int(multiprocessing.cpu_count() / 2)
+	# #cpu_count = multiprocessing.cpu_count()
+	# print("Running on {} CPU cores.".format(cpu_count))
+	# pool = multiprocessing.Pool(processes=cpu_count)
+	# toolbox.register("map", pool.map)
 
 	pop = toolbox.population(n=MU)
 	hof = tools.HallOfFame(1)
@@ -186,7 +204,7 @@ def main():
 	
 	return pop, log, hof
 
-def GenerateProgressFigure(gen_vals, best_vals, avg_vals):
+def GenerateProgressFigure(run_number, gen_vals, best_vals, avg_vals):
 	plt.plot(gen_vals, best_vals)
 	plt.plot(gen_vals, avg_vals)
 	#plt.plot(gen_vals, worst_vals)
@@ -195,10 +213,10 @@ def GenerateProgressFigure(gen_vals, best_vals, avg_vals):
 	plt.ylabel("Distance")
 	#plt.show()
 	plt.title("Progress")
-	plt.savefig("master-progress.png")
+	plt.savefig("master-progress-{}.png".format(run_number))
 	plt.clf()
 
-def GenerateBestPath(wh_point, path):
+def GenerateBestPath(wh_point, path, run_number):
 	ordered_points = [wh_point] + [ points[x] for x in path]
 	distance = sum([ GetDistance(p1, p2) for p1, p2 in [ (ordered_points[i-1], ordered_points[i]) for i in range(len(ordered_points))] ])
 
@@ -209,23 +227,27 @@ def GenerateBestPath(wh_point, path):
 		plt.plot( [ x[0] for x in pair ], [ x[1] for x in pair ], color="g")
 	plt.scatter( [ x[0] for x in ordered_points if x != wh_point ], [ x[1] for x in ordered_points if x != wh_point ], color="g", marker="o")
 	plt.scatter([wh_point[0]], [wh_point[1]], color="r", marker="o")
-	plt.title("Distance: {}".format(distance))
-	plt.savefig("master-solution.png")
+	plt.title("({}) Distance: {}".format(run_number, distance))
+	plt.savefig("master-solution-{}.png".format(run_number))
 	plt.clf()
 
 if __name__ == "__main__":
-	_, logbook, hof = main()
+	for i in range(1):
+		print("{}. run".format(i+1))
+		_, logbook, hof = main()
 
-	print("Generating progress figure...")
-	gen_vals   = [ x["gen"] for x in logbook ]
-	best_vals  = [ x["min"] for x in logbook ]
-	avg_vals   = [ x["avg"] for x in logbook ]
-	GenerateProgressFigure(gen_vals, best_vals, avg_vals)
+		print("Generating progress figure...")
+		gen_vals   = [ x["gen"] for x in logbook ]
+		best_vals  = [ x["min"] for x in logbook ]
+		avg_vals   = [ x["avg"] for x in logbook ]
+		GenerateProgressFigure(i, gen_vals, best_vals, avg_vals)
 
-	best = hof[0]
-	print(best)
-	wh_x, wh_y = best[0], best[1]
-	path = paths[str(best)]
-	GenerateBestPath((wh_x, wh_y), path)
-	print("Best position for warehouse: {}".format( (wh_x, wh_y) ))
-	print("Best value: {}".format(best.fitness))
+		best = hof[0]
+		wh_x, wh_y = best[0], best[1]
+		path = best.path
+		print(best)
+		GenerateBestPath((wh_x, wh_y), path, i)
+		with open("best.csv","a") as best_file:
+			print("({},{});{}".format(wh_x, wh_y, best.fitness), file=best_file)
+		print("Best position for warehouse: {}".format( (wh_x, wh_y) ))
+		print("Best value: {}".format(best.fitness))
