a692d0c2a500b12fbac4dba6995b0120720f9648
Marek Nagy <62247071@cuni.cz>
2021-03-30 09:22:53 +0200

    Added ToString and made values bemore likely to be added to closer warehouses

--- csharp-console/csharp-console/Chromosomes/WarehousesChromosome.cs
+++ csharp-console/csharp-console/Chromosomes/WarehousesChromosome.cs
@@ -80,19 +80,30 @@ namespace csharp_console
 			foreach (var coord in coords)
 			{
 				var rand_value = rand.NextDouble();
-				var val = 1d / warehouses.Length;
+				//var val = 1d / warehouses.Length;
+
+				// calculate probabilities
+				double[] distances = new double[warehouses.Length];
+				for (int i = 0; i < warehouses.Length; i++)
+				{
+					var wh = warehouses[i];
+					distances[i] = Evaluation.EuklidianDistance(coord, wh.Point);
+				}
+				var distancesSum = distances.Sum();
+				var probabilities = distances.Select(x => x / distancesSum).ToArray();
+
 				for (int i = 0; i < warehouses.Length; i++)
 				{
 					if (i < warehouses.Length - 1)
 					{
-						if (rand_value < val)
+						if (rand_value < probabilities[i])
 						{
 							warehouses[i].InsertToCar(rand.Next(warehouses[i].CarsAmount), coord);
 							break;
 						}
 						else
 						{
-							rand_value -= val;
+							rand_value -= probabilities[i];
 						}
 					}
 					else
@@ -120,14 +131,39 @@ namespace csharp_console
 			double[] values = warehouses.Select(wh => wh.Fitness).ToArray();
 			Fitness = values.Max();
 		}
-		//async internal Task ComputeFitness()
-		//{
-		//	try{
-		//		await ComputeFitness( Evaluation.EuklidianDistance );
-		//	}
-		//	catch (Exception e) {
-		//		int a = 5;
-		//	}
-		//}
+        //async internal Task ComputeFitness()
+        //{
+        //	try{
+        //		await ComputeFitness( Evaluation.EuklidianDistance );
+        //	}
+        //	catch (Exception e) {
+        //		int a = 5;
+        //	}
+        //}
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+			sb.AppendLine(this.Fitness.ToString());
+			foreach (var warehouse in this.warehouses)
+			{
+
+				sb.AppendLine($"{warehouse.Point.X};{warehouse.Point.Y}");
+				//sb.AppendLine( warehouse.CarRoutes.Select(cr => cr.Count > 0 ? 1 : 0).Sum().ToString() );
+				foreach (var cr in warehouse.CarRoutes)
+				{
+					if (cr.Count == 0) continue;
+
+					for (int j = 0; j < cr.Count; j++)
+					{
+						if (j > 0)
+							sb.Append(';');
+						sb.Append($"{cr[j].X};{cr[j].Y}");
+					}
+					sb.AppendLine();
+				}
+				sb.AppendLine("###");
+			}
+			return sb.ToString();
+        }
 	}
 }
