62f51bef2e35b0e2c727d0085c4685961d205a4a
Marek Nagy <62247071@cuni.cz>
2021-05-15 19:28:26 +0200

    Added default values to Config variables

--- csharp-console/csharp-console/Config.cs
+++ csharp-console/csharp-console/Config.cs
@@ -12,15 +12,15 @@ namespace csharp_console
 	{
 		public int WarehousesAmount { get; set; }
 		public int[] CarsAmount { get; set; }
-		public int NGen { get; set; }
-		public int PopulationSize { get; set; }
+		public int NGen { get; set; } = 50;
+		public int PopulationSize { get; set; } = 10;
 		public double WarehouseMutProb { get; set; }
 		public double PointWarehouseMutProb { get; set; }
 		public double RouteMutProb { get; set; }
-		public int Runs { get; set; }
-		public int MaxParallelRequests { get; set; }
-		public string ServerHost { get; set; }
-		public int ServerPort { get; set; }
+		public int Runs { get; set; } = 10;
+		public int MaxParallelRequests { get; set; } = 5;
+		public string ServerHost { get; set; } = "localhost";
+		public int ServerPort { get; set; } = 5_000;
 		public static Config FromJson(string configFile)
 		{
 			string text = File.ReadAllText(configFile);
